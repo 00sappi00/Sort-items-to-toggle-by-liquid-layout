a2ea16fb82d862f3ed16e79efaf4411f0e3e4c86
00sappi00 <00sappi00@gmail.com>
2019-01-27 13:45:40 +0900

    gh-pagesブランチを作成

---
+++ event.js
@@ -0,0 +1,69 @@
+'use strict';
+
+  var mql = window.matchMedia('(min-width: 960px)');
+
+  var widthChange = function(mq){
+    if(mq.matches){
+      $('.container').empty();
+      $.ajax({
+          type: 'GET',
+          url: "./data.json",
+          dataType: "json",
+          success: function(data) {
+            pcEventSort(data);
+          },
+          error: function(){
+            console.log("error");
+          }
+      });
+    }else{
+      $('.container').empty();
+      $.ajax({
+          type: 'GET',
+          url: "./data.json",
+          dataType: "json",
+          success: function(data) {
+            spEventSort(data);
+          },
+          error: function(){
+            console.log("error");
+          }
+      });
+    }
+  }
+
+  mql.addListener(widthChange);
+  widthChange(mql);
+
+var pcEventSort = function(data){
+  var grid = "<div class='grid clearfix'><div class='grid-col'></div><div class='grid-col'></div><div class='grid-col'></div></div>";
+  $('.container').append(grid);
+  for(var i = 0; i < data.event.length; i++){
+    var el = "<div class='grid-item grid-item0" + (i+1) + "'>";
+    el += "<div class='grid-img'><img src='" + data.event[i].image + "'></div>";
+    el += "<dl><dt>" + data.event[i].event_name + "</dt>";
+    el += "<dd style='display:none;'>" + data.event[i].detail + "</dd>";
+    el += "</dl></div>"
+
+    if( (i+1)%3 == 1){
+      $('.grid-col:nth-child(1)').append(el);
+    }else if( (i+1)%3 == 2){
+      $('.grid-col:nth-child(2)').append(el);
+    }else if( (i+1)%3 == 0){
+      $('.grid-col:nth-child(3)').append(el);
+    }
+  }
+}
+var spEventSort = function(data){
+  var grid = "<div class='grid clearfix'></div>";
+  $('.container').append(grid);
+  for(var i = 0; i < data.event.length; i++){
+    var el = "<div class='grid-item grid-item0" + (i+1) + "'>";
+    el += "<div class='grid-img'><img src='" + data.event[i].image + "'></div>";
+    el += "<dl><dt>" + data.event[i].event_name + "</dt>";
+    el += "<dd style='display:none;'>" + data.event[i].detail + "</dd>";
+    el += "</dl></div>"
+    $('.grid').append(el);
+
+  }
+}
